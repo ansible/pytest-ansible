8966aa309653a26afeb1cdcda4a40806b98c8712
James Laska <jlaska@ansible.com>
2015-02-16 13:51:49 -0500

    Bump version

--- pytest_ansible.py
+++ pytest_ansible.py
@@ -10,7 +10,7 @@ import ansible.errors
 import ansible.utils
 from urlparse import urlparse
 
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = "James Laska"
 __author_email__ = "<jlaska@ansible.com>"
 
