dc8daffd5ab4a0d31d25a06fd4a5f7d964db9bd1
James Laska <jlaska@ansible.com>
2015-04-20 14:05:16 -0400

    Updated tests

--- test_pytest_ansible.py
+++ test_pytest_ansible.py
@@ -1,195 +1,368 @@
 import pytest
-from pytest_ansible.errors import (AnsibleHostUnreachable, AnsibleNoHostsMatch)
+import ansible
+from _pytest.main import EXIT_OK, EXIT_TESTSFAILED, EXIT_USAGEERROR
 
 
 pytest_plugins = 'pytester'
 
 
-def assert_ping_response_success(data):
-    return assert_ping_response(data, failed=False)
+class PyTestOption(object):
+
+    def __init__(self, config, testdir):
+        self.config = config
+
+        # Create inventory file
+        self.inventory = testdir.makefile('.ini', '''
+            [local]
+            localhost ansible_connection=local ansible_python_interpreter='/usr/bin/env python'
+            127.0.0.2 ansible_connection=local ansible_python_interpreter='/usr/bin/env python'
+            127.0.0.3 ansible_connection=local ansible_python_interpreter='/usr/bin/env python'
+            127.0.0.4 ansible_connection=local ansible_python_interpreter='/usr/bin/env python'
+            127.0.0.5 ansible_connection=local ansible_python_interpreter='/usr/bin/env python'
+
+            [unreachable]
+            unreachable-host-1.example.com
+            unreachable-host-2.example.com
+            unreachable-host-3.example.com
+        ''')
 
 
-def assert_ping_response_failure(data):
-    return assert_ping_response(data, failed=True)
+    @property
+    def args(self):
+        args = list()
+        args.append('--tb')
+        args.append('native')
+        return args
 
 
-def assert_ping_response(data, failed):
-
-    if failed:
-        assert 'failed' in data
-        assert data['failed']
-    else:
-        assert 'failed' not in data
-        assert 'invocation' in data
-        assert 'module_name' in data['invocation']
-        assert 'ping' == data['invocation']['module_name']
-
-
-def test_ping(ansible_module):
-    '''Verify the ansible ping module across all inventory
+@pytest.fixture()
+def option(request, testdir):
+    '''Returns an instance of PyTestOption to help tests pass parameters and
+    use a common inventory file.
     '''
-    exc_info = pytest.raises(AnsibleHostUnreachable, ansible_module.ping)
-    (contacted, dark) = exc_info.value.results
-
-    # assert contacted hosts ...
-    assert contacted
-    assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('!unreachable'))
-    for result in contacted.values():
-        assert_ping_response_success(result)
-
-    # assert dark hosts ...
-    assert dark
-    assert len(dark) == len(ansible_module.inventory_manager.list_hosts('unreachable'))
-    for result in dark.values():
-        assert_ping_response_failure(result)
+    return PyTestOption(request.config, testdir)
 
 
-@pytest.mark.ansible(host_pattern='localhost')
-def test_ping_localhost(ansible_module):
-    '''Verify the ansible ping module while specifying a host_pattern:localhost
+def test_report_header(testdir, option):
+    '''Verify the expected ansible version in the pytest report header.
     '''
-    result = ansible_module.ping()
 
-    # assert contacted hosts ...
-    assert len(result) == 1
-    for contacted in result.values():
-        assert_ping_response_success(contacted)
+    result = testdir.runpytest(*option.args)
+    assert result.ret == EXIT_OK
+    result.stdout.fnmatch_lines([
+        'ansible: %s' % ansible.__version__,
+    ])
 
 
-@pytest.mark.ansible(host_pattern='localhost')
-def test_ping_limit_localhost(ansible_module):
-    '''Verify the ansible ping module while specifying a host_pattern:localhost
+def test_params_not_required(testdir, option):
+    '''Verify the ansible parameters are not required if the fixture is not used.
     '''
-    contacted = ansible_module.ping()
 
-    # assert contacted hosts ...
-    assert contacted
-    assert len(contacted) == 1
-    for result in contacted.values():
-        assert_ping_response_success(result)
-
-
-@pytest.mark.ansible(host_pattern='unreachable')
-def test_ping_limit_unreachable(ansible_module):
-    '''Verify the ansible ping module while specifying a host_pattern:unreachable
+    src = '''
+        import pytest
+        def test_func():
+            assert True
     '''
-    exc_info = pytest.raises(AnsibleHostUnreachable, ansible_module.ping)
-    (contacted, dark) = exc_info.value.results
-
-    # assert no contacted hosts ...
-    assert not contacted, "%d hosts were contacted, expected %d" \
-        % (len(contacted), 0)
-
-    # assert dark hosts ...
-    assert dark
-    assert len(dark) == len(ansible_module.inventory_manager.list_hosts('unreachable'))
-    for result in dark.values():
-        assert_ping_response_failure(result)
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args)
+    assert result.ret == EXIT_OK
 
 
-@pytest.mark.ansible(host_pattern='!unreachable')
-def test_ping_not_unreachable(ansible_module):
-    '''Verify the ansible ping module while specifying a host_pattern:!unreachable
+def test_params_required(testdir, option):
+    '''Verify the ansible parameters are required if the fixture is used.
     '''
-    contacted = ansible_module.ping()
 
-    # assert contacted hosts ...
-    assert contacted
-    assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('!unreachable'))
-    for result in contacted.values():
-        assert_ping_response_success(result)
-
-
-@pytest.mark.ansible(connection='local')
-def test_ping_connection_local(ansible_module):
-    '''Verify the ansible ping module while specifying a connection:local
+    src = '''
+        import pytest
+        def test_func(ansible_module):
+            assert True
     '''
-    contacted = ansible_module.ping()
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args)
+    assert result.ret == EXIT_USAGEERROR
+    result.stderr.fnmatch_lines([
+        'ERROR: Missing required parameter --ansible-host-pattern',
+    ])
 
-    # assert contacted hosts ...
-    assert contacted
-    assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('all'))
-    for result in contacted.values():
-        assert_ping_response_success(result)
 
-
-@pytest.mark.ansible(inventory='local,', connection='local')
-def test_ping_inventory_local(ansible_module):
-    '''Verify the ansible ping module while specifying a inventory:local, and connection:local
+def test_params_required_with_host_generator(testdir, option):
+    '''Verify the ansible parameters are required if the fixture is used.
     '''
-    contacted = ansible_module.ping()
 
-    # assert contacted hosts ...
-    assert contacted
-    assert len(contacted) == 1
-    for result in contacted.values():
-        assert_ping_response_success(result)
-
-
-@pytest.mark.ansible(host_pattern='localhost', sudo=True)
-def test_ping_sudo(ansible_module):
-    '''Verify the ansible ping module while specifying a host_pattern:localhost and sudo:true
+    src = '''
+        import pytest
+        def test_func(ansible_host):
+            assert True
     '''
-    contacted = ansible_module.ping()
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args)
+    assert result.ret == EXIT_TESTSFAILED
+    result.stdout.fnmatch_lines([
+        'collected 0 items / 1 errors',
+        'E   UsageError: Missing required parameter --ansible-host-pattern',
+    ])
 
-    # assert contacted hosts ...
-    assert contacted
-    assert len(contacted) == 1
-    for result in contacted.values():
-        assert_ping_response_failure(result)
 
-
-@pytest.mark.ansible(host_pattern='random_limit_that_has_no_matches')
-def test_ping_limit_no_matching_host(ansible_module):
-    '''Verify the ansible ping fails when specifying a host_pattern that does not match
+def test_params_required_with_group_generator(testdir, option):
+    '''Verify the ansible parameters are required if the fixture is used.
     '''
-    with pytest.raises(AnsibleNoHostsMatch):
-        ansible_module.ping()
 
-
-def test_ansible_facts(ansible_facts, ansible_module):
-    '''Verify that the ansible_facts fixture behaves as expected
+    src = '''
+        import pytest
+        def test_func(ansible_group):
+            assert True
     '''
-    # assert contacted hosts ...
-    assert ansible_facts
-    assert len(ansible_facts) == len(ansible_module.inventory_manager.list_hosts('!unreachable'))
-    for host_facts in ansible_facts.values():
-        assert 'ansible_facts' in host_facts
-        assert 'ansible_os_family' in host_facts['ansible_facts']
-        assert 'ansible_distribution' in host_facts['ansible_facts']
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args)
+    assert result.ret == EXIT_TESTSFAILED
+    result.stdout.fnmatch_lines([
+        'collected 0 items / 1 errors',
+        'E   UsageError: Missing required parameter --ansible-host-pattern',
+    ])
 
 
-def test_ansible_host(ansible_host, ansible_module):
-    '''Verify the parameterized fixture ansible_host restricts inventory properly
+def test_params_required_with_inventory_without_host_pattern(testdir, option):
+    src = '''
+        import pytest
+        def test_func(ansible_module):
+            assert True
     '''
-    # assert inventory is restricted to a single host matching 'ansible_host'
-    assert ansible_module.inventory_manager.list_hosts() == [ansible_host]
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', 'local,'])
+    assert result.ret == EXIT_USAGEERROR
+    result.stderr.fnmatch_lines([
+        'ERROR: Missing required parameter --ansible-host-pattern',
+    ])
 
 
-def test_ansible_group(ansible_group, ansible_module):
-    '''Verify the parameterized fixture ansible_group restricts inventory properly
+def test_params_required_with_bogus_inventory(testdir, option):
+    src = '''
+        import pytest
+        def test_func(ansible_module):
+            assert True
     '''
-    # assert that only hosts from 'ansible_group' were found
-    for host in ansible_module.inventory_manager.get_hosts():
-        assert ansible_group in [g.name for g in host.get_groups()]
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', 'no such file', '--ansible-host-pattern', 'all'])
+    assert result.ret == EXIT_TESTSFAILED
+    result.stdout.fnmatch_lines([
+        'UsageError: Unable to find an inventory file, specify one with -i ?',
+    ])
 
 
-@pytest.mark.ansible(host_pattern='localhost', connection='local')
-class Test_Fixture_Scope(object):
-    '''Verify that pytest.mark.ansible applies to all class methods
+def test_params_required_without_inventory_with_host_pattern(testdir, option):
+    src = '''
+        import pytest
+        def test_func(ansible_module):
+            assert True
     '''
-    def test_ping_single_host(self, ansible_module):
-        # ping
-        contacted = ansible_module.ping()
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-host-pattern', 'all'])
+    assert result.ret == EXIT_TESTSFAILED
+    result.stdout.fnmatch_lines([
+        'UsageError: Unable to find an inventory file, specify one with -i ?',
+    ])
 
-        # assert a single contacted host ...
-        assert contacted and len(contacted) == 1
-        assert 'localhost' in contacted
 
-    def test_ping_single_host_again(self, ansible_module):
-        # ping
-        contacted = ansible_module.ping()
+def test_contacted_with_params(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        def test_func(ansible_module):
+            contacted = ansible_module.ping()
 
-        # assert a single contacted host ...
-        assert contacted and len(contacted) == 1
-        assert 'localhost' in contacted
+            # assert contacted hosts ...
+            assert contacted
+            assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('local'))
+            for result in contacted.values():
+                assert 'failed' not in result
+                assert 'invocation' in result
+                assert 'module_name' in result['invocation']
+                assert 'ping' == result['invocation']['module_name']
+
+    '''
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', str(option.inventory), '--ansible-host-pattern', 'local'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_contacted_with_params_and_inventory_marker(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        @pytest.mark.ansible(inventory='%s')
+        def test_func(ansible_module):
+            contacted = ansible_module.ping()
+
+            # assert contacted hosts ...
+            assert contacted
+            assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('local'))
+            for result in contacted.values():
+                assert 'failed' not in result
+                assert 'invocation' in result
+                assert 'module_name' in result['invocation']
+                assert 'ping' == result['invocation']['module_name']
+
+    ''' % str(option.inventory)
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-host-pattern', 'local'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_contacted_with_params_and_host_pattern_marker(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        @pytest.mark.ansible(host_pattern='local')
+        def test_func(ansible_module):
+            contacted = ansible_module.ping()
+
+            # assert contacted hosts ...
+            assert contacted
+            assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('local'))
+            for result in contacted.values():
+                assert 'failed' not in result
+                assert 'invocation' in result
+                assert 'module_name' in result['invocation']
+                assert 'ping' == result['invocation']['module_name']
+
+    '''
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', str(option.inventory), '--ansible-host-pattern', 'unreachable'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_contacted_with_params_and_inventory_host_pattern_marker(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        @pytest.mark.ansible(inventory='%s', host_pattern='local')
+        def test_func(ansible_module):
+            contacted = ansible_module.ping()
+
+            # assert contacted hosts ...
+            assert contacted
+            assert len(contacted) == len(ansible_module.inventory_manager.list_hosts('local'))
+            for result in contacted.values():
+                assert 'failed' not in result
+                assert 'invocation' in result
+                assert 'module_name' in result['invocation']
+                assert 'ping' == result['invocation']['module_name']
+
+    ''' % str(option.inventory)
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', '/dev/null', '--ansible-host-pattern', 'unreachable'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_dark_with_params(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        from pytest_ansible.errors import (AnsibleHostUnreachable, AnsibleNoHostsMatch)
+        def test_func(ansible_module):
+            exc_info = pytest.raises(AnsibleHostUnreachable, ansible_module.ping)
+            (contacted, dark) = exc_info.value.results
+
+            # assert no contacted hosts ...
+            assert not contacted, "%d hosts were contacted, expected %d" \
+                % (len(contacted), 0)
+
+            # assert dark hosts ...
+            assert dark
+            assert len(dark) == len(ansible_module.inventory_manager.list_hosts('unreachable'))
+            for result in dark.values():
+                assert 'failed' in result
+                assert result['failed']
+    '''
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', str(option.inventory), '--ansible-host-pattern', 'unreachable'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_dark_with_params_and_inventory_marker(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        from pytest_ansible.errors import (AnsibleHostUnreachable, AnsibleNoHostsMatch)
+        @pytest.mark.ansible(inventory='{inventory}')
+        def test_func(ansible_module):
+            exc_info = pytest.raises(AnsibleHostUnreachable, ansible_module.ping)
+            (contacted, dark) = exc_info.value.results
+
+            # assert no contacted hosts ...
+            assert not contacted, "%d hosts were contacted, expected %d" \
+                % (len(contacted), 0)
+
+            # assert dark hosts ...
+            assert dark
+            assert len(dark) == len(ansible_module.inventory_manager.list_hosts('unreachable'))
+            for result in dark.values():
+                assert 'failed' in result
+                assert result['failed']
+    '''.format(inventory=str(option.inventory))
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-host-pattern', 'unreachable'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_dark_with_params_and_host_pattern_marker(testdir, option):
+    '''FIXME
+    '''
+    src = '''
+        import pytest
+        from pytest_ansible.errors import (AnsibleHostUnreachable, AnsibleNoHostsMatch)
+        @pytest.mark.ansible(host_pattern='unreachable')
+        def test_func(ansible_module):
+            exc_info = pytest.raises(AnsibleHostUnreachable, ansible_module.ping)
+            (contacted, dark) = exc_info.value.results
+
+            # assert no contacted hosts ...
+            assert not contacted, "%d hosts were contacted, expected %d" \
+                % (len(contacted), 0)
+
+            # assert dark hosts ...
+            assert dark
+            assert len(dark) == len(ansible_module.inventory_manager.list_hosts('unreachable'))
+            for result in dark.values():
+                assert 'failed' in result
+                assert result['failed']
+                assert result['msg'].startswith('SSH Error: ssh: Could not resolve hostname')
+    '''
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', str(option.inventory), '--ansible-host-pattern', 'local'])
+    assert result.ret == EXIT_OK
+    assert result.parseoutcomes()['passed'] == 1
+
+
+def test_dark_with_debug_enabled(testdir, option):
+    '''Verify that when --ansible-debug is provide, additional output is provided upon host failure.
+    '''
+    src = '''
+        import pytest
+        from pytest_ansible.errors import AnsibleHostUnreachable
+        def test_func(ansible_module):
+            ansible_module.ping()
+    '''
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*option.args + ['--ansible-inventory', str(option.inventory),
+                                               '--ansible-host-pattern', 'unreachable',
+                                               '--ansible-debug'])
+    assert result.ret == EXIT_TESTSFAILED
+    assert result.parseoutcomes()['failed'] == 1
+    result.stdout.fnmatch_lines([
+        '*ESTABLISH CONNECTION FOR USER: *',
+        '*REMOTE_MODULE ping',
+        '*EXEC ssh *',
+    ])
