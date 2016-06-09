e3ee2cb4746e1f1e315ed944e776d1ed3dfdb67f
James Laska <jlaska@redhat.com>
2016-06-09 12:02:38 -0400

    Update old fixtures to use new-style host_mgr object

--- pytest_ansible/fixtures.py
+++ pytest_ansible/fixtures.py
@@ -1,5 +1,4 @@
 import pytest
-import warnings
 import logging
 from .errors import AnsibleHostUnreachable
 
@@ -17,36 +16,14 @@ log = logging.getLogger(__name__)
 log.addHandler(NullHandler())
 
 
-@pytest.fixture(scope='class')
-def ansible_module_cls(request):
-    '''
-    Return AnsibleV1Module instance with class scope.
-    '''
-    warnings.warn("Use of ansible_module_cls is deprecated and will be removed in a future release", DeprecationWarning)
-    ansible_helper = request.config.pluginmanager.getplugin("ansible")
-    return ansible_helper.initialize(request)
-
-
 @pytest.fixture(scope='function')
 def ansible_module(request):
     '''
     Return AnsibleV1Module instance with function scope.
     '''
     ansible_helper = request.config.pluginmanager.getplugin("ansible")
-    return ansible_helper.initialize(request)
-
-
-@pytest.fixture(scope='function')
-def ansible_facts_cls(ansible_module_cls):
-    '''
-    Return ansible_facts dictionary
-    '''
-    warnings.warn("Use of ansible_facts_cls is deprecated and will be removed in a future release", DeprecationWarning)
-    try:
-        return ansible_module_cls.setup()
-    except AnsibleHostUnreachable, e:
-        log.warning("Hosts unreachable: %s" % e.dark.keys())
-        return e.contacted
+    hosts_mgr = ansible_helper.initialize(request)
+    return hosts_mgr.all
 
 
 @pytest.fixture(scope='function')
@@ -55,7 +32,7 @@ def ansible_facts(ansible_module):
     Return ansible_facts dictionary
     '''
     try:
-        return ansible_module.setup()
+        return ansible_module.all.setup()
     except AnsibleHostUnreachable, e:
         log.warning("Hosts unreachable: %s" % e.dark.keys())
         return e.contacted
