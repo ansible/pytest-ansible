a1708d6c4e27a998026813457db0cf93a2bef612
James Laska <jlaska@redhat.com>
2016-06-30 12:55:31 -0400

    Remove AnsibleHostUnreachable exception

--- pytest_ansible/errors.py
+++ pytest_ansible/errors.py
@@ -5,20 +5,6 @@ class AnsibleNoHostsMatch(ansible.errors.AnsibleError):
     pass
 
 
-class AnsibleHostUnreachable(ansible.errors.AnsibleError):
-
-    '''FIXME'''
-
-    def __init__(self, msg, dark=None, contacted=None):
-        super(AnsibleHostUnreachable, self).__init__(msg)
-        self.contacted = contacted
-        self.dark = dark
-
-    @property
-    def results(self):
-        return (self.contacted, self.dark)
-
-
 class AnsibleConnectionFailure(ansible.errors.AnsibleError):
 
     '''FIXME'''
@@ -27,10 +13,6 @@ class AnsibleConnectionFailure(ansible.errors.AnsibleError):
         super(AnsibleConnectionFailure, self).__init__(msg)
         self.contacted = contacted
         self.dark = dark
-
-    @property
-    def results(self):
-        return (self.contacted, self.dark)
 
 
 class AnsibleModuleError(ansible.errors.AnsibleError):
