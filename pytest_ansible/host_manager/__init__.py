db2f64a506ebdca2040fbb93f2610e1946690f51
James Laska <jlaska@redhat.com>
2016-06-30 12:55:56 -0400

    Improve host_manager slice support (with tests)

--- pytest_ansible/host_manager/__init__.py
+++ pytest_ansible/host_manager/__init__.py
@@ -28,7 +28,6 @@ class BaseHostManager(object):
     def __init__(self, *args, **kwargs):
         self.options = kwargs
 
-        # FIXME - move to has_required_kwargs method
         self.has_required_kwargs(**kwargs)
 
         # Initialize ansible inventory manager
@@ -41,15 +40,29 @@ class BaseHostManager(object):
         for kwarg in self._required_kwargs:
             assert kwarg in self.options, "Missing required keyword argument '%s'" % kwarg
 
+    def has_matching_inventory(self, host_pattern):
+        '''Return whether any matching ansible inventory is found for the provided host_pattern.'''
+        try:
+            return len(self.options['inventory_manager'].list_hosts(host_pattern)) > 0 or \
+                host_pattern in self.options['inventory_manager'].groups
+        except ansible.errors.AnsibleError:
+            return False
+
     def __getitem__(self, item):
         # Handle slicing
         if isinstance(item, slice):
-            item = "*[(start):(stop)]".format(item)
+            new_item = "all["
+            if item.start is not None:
+                new_item += str(item.start)
+            new_item += ':'
+            if item.stop is not None:
+                new_item += str(item.stop)
+            item = new_item + ']'
 
         if item in self.__dict__:
             return self.__dict__[item]
         else:
-            if len(self.options['inventory_manager'].list_hosts(item)) == 0 and item not in self.options['inventory_manager'].groups:
+            if not self.has_matching_inventory(item):
                 raise KeyError(item)
             else:
                 self.options['host_pattern'] = item
@@ -59,7 +72,7 @@ class BaseHostManager(object):
         """Maps values to attributes.
         Only called if there *isn't* an attribute with this name
         """
-        if len(self.options['inventory_manager'].list_hosts(attr)) == 0 and attr not in self.options['inventory_manager'].groups:
+        if not self.has_matching_inventory(attr):
             raise AttributeError("type HostManager has no attribute '%s'" % attr)
         else:
             self.options['host_pattern'] = attr
