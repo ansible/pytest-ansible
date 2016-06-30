e97eb071de7f779d685d275788cfa265dc4c6027
James Laska <jlaska@redhat.com>
2016-06-30 12:56:44 -0400

    Reduce test duplication

--- tests/test_adhoc_result.py
+++ tests/test_adhoc_result.py
@@ -1,26 +1,9 @@
 import pytest
 from pytest_ansible.results import ModuleResult
+from conftest import ALL_HOSTS
 
 
-positive_host_patterns = {
-    'all': 2,
-    '*': 2,
-    'localhost': 1,
-    'local*': 1,
-    'local*:&*host': 1,
-    '!localhost': 1,
-    'all[0]': 1,
-    'all[-1]': 1,
-    '*[0:1]': 2,  # this is confusing, but how host slicing works
-    '*[0:]': 2,
-}
-negative_host_patterns = {
-    'none': 0,
-    'all[8:]': 0,
-}
-
-valid_hosts = ('localhost', 'another_host')
-invalid_hosts = ('none', 'all', '*', 'local*')
+invalid_hosts = ['none', 'all', '*', 'local*']
 
 
 @pytest.fixture()
@@ -29,16 +12,16 @@ def adhoc_result(hosts):
 
 
 def test_len(adhoc_result):
-    assert len(adhoc_result) == 2
+    assert len(adhoc_result) == len(ALL_HOSTS)
 
 
 def test_keys(adhoc_result):
     sorted_keys = adhoc_result.keys()
     sorted_keys.sort()
-    assert sorted_keys == ['another_host', 'localhost']
+    assert sorted_keys == ALL_HOSTS
 
 
-@pytest.mark.parametrize("host", valid_hosts)
+@pytest.mark.parametrize("host", ALL_HOSTS)
 def test_contains(adhoc_result, host):
     assert host in adhoc_result
 
@@ -48,7 +31,7 @@ def test_not_contains(adhoc_result, host):
     assert host not in adhoc_result
 
 
-@pytest.mark.parametrize("host_pattern", valid_hosts)
+@pytest.mark.parametrize("host_pattern", ALL_HOSTS)
 def test_getitem(adhoc_result, host_pattern):
     assert adhoc_result[host_pattern]
     assert isinstance(adhoc_result[host_pattern], ModuleResult)
@@ -60,7 +43,7 @@ def test_not_getitem(adhoc_result, host_pattern):
         assert adhoc_result[host_pattern]
 
 
-@pytest.mark.parametrize("host_pattern", valid_hosts)
+@pytest.mark.parametrize("host_pattern", ALL_HOSTS)
 def test_getattr(adhoc_result, host_pattern):
     assert hasattr(adhoc_result, host_pattern)
     assert isinstance(adhoc_result[host_pattern], ModuleResult)
@@ -78,7 +61,8 @@ def test_connection_failure_v1():
     from pytest_ansible.host_manager import get_host_manager
     from pytest_ansible.errors import AnsibleConnectionFailure
     hosts = get_host_manager(inventory='unknown.example.com,', connection='smart')
-    exc_info = pytest.raises(AnsibleConnectionFailure, hosts.all.ping)
+    with pytest.raises(AnsibleConnectionFailure) as exc_info:
+        hosts.all.ping()
     # Assert message
     assert exc_info.value.message == "Host unreachable"
     # Assert contacted
@@ -99,7 +83,8 @@ def test_connection_failure_v2():
     from pytest_ansible.host_manager import get_host_manager
     from pytest_ansible.errors import AnsibleConnectionFailure
     hosts = get_host_manager(inventory='unknown.example.com,', connection='smart')
-    exc_info = pytest.raises(AnsibleConnectionFailure, hosts.all.ping)
+    with pytest.raises(AnsibleConnectionFailure) as exc_info:
+        hosts.all.ping()
     # Assert message
     assert exc_info.value.message == "Host unreachable"
     # Assert contacted
