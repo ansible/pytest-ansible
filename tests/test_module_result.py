bb02cc84f42f5fc061526432a65dca34af009a8e
James Laska <jlaska@redhat.com>
2016-07-01 09:10:49 -0400

    Comment out unused portion of _check_key
    It appears that the `_check_key` method on ansible runs against a task.
    Tasks support loops, which pytest_ansible does not yet.  Therefore, this
    should be safe to remove for now.

--- tests/test_module_result.py
+++ tests/test_module_result.py
@@ -24,27 +24,72 @@ invalid_hosts = ('none', 'all', '*', 'local*')
 
 
 @pytest.fixture()
-def module_result_ok():
-    return ModuleResult(**{'invocation': {'module_name': u'debug', 'module_args': {'msg': u'testing'}}, 'msg':
-                           u'testing', 'changed': False, '_ansible_verbose_always': True, '_ansible_no_log': False})
+def module_result_ok(request):
+    return ModuleResult(
+        **{
+            'invocation': {
+                'module_name': u'debug',
+                'module_args': {
+                    'msg': u'testing'
+                }
+            },
+            'msg': u'testing',
+            'changed': False,
+            '_ansible_verbose_always': True,
+            '_ansible_no_log': False
+        }
+    )
 
 
 @pytest.fixture()
 def module_result_failed():
-    return ModuleResult(**{'invocation': {'module_name': u'fail', 'module_args': {}}, 'failed': True, 'changed': False,
-                           '_ansible_no_log': False, 'msg': u'Failed as requested from task'})
+    return ModuleResult(
+        **{
+            'invocation': {
+                'module_name': u'fail',
+                'module_args': {}
+            },
+            'failed': True,
+            'changed': False,
+            '_ansible_no_log': False,
+            'msg': u'Failed as requested from task'
+        }
+    )
 
 
 @pytest.fixture()
-def module_result_changed():
-    return ModuleResult(**{u'changed': True, u'end': u'2016-06-17 21:32:54.877597', '_ansible_no_log': False, u'stdout':
-                           u'Fri Jun 17 21:32:54 EDT 2016', u'cmd': [u'date'], u'rc': 0, u'start':
-                           u'2016-06-17 21:32:54.873429', u'stderr': u'', u'delta': u'0:00:00.004168', 'invocation':
-                           {'module_name': u'command', u'module_args': {u'creates': None, u'executable': None,
-                                                                        u'_uses_shell': False, u'_raw_params': u'date',
-                                                                        u'removes': None, u'warn': True, u'chdir':
-                                                                        None}},
-                           'stdout_lines': [u'Fri Jun 17 21:32:54 EDT 2016'], u'warnings': []})
+def module_result_changed(request):
+    return ModuleResult(
+        **{
+            'changed': True,
+            'end': '2016-06-17 21:32:54.877597',
+            '_ansible_no_log': False,
+            'stdout': 'Fri Jun 17 21:32:54 EDT 2016',
+            'cmd': [
+                'date'
+            ],
+            'rc': 0,
+            'start': '2016-06-17 21:32:54.873429',
+            'stderr': '',
+            'delta': '0:00:00.004168',
+            'invocation': {
+                'module_name': 'command',
+                'module_args': {
+                    'creates': None,
+                    'executable': None,
+                    '_uses_shell': False,
+                    '_raw_params': 'date',
+                    'removes': None,
+                    'warn': True,
+                    'chdir': None
+                }
+            },
+            'stdout_lines': [
+                'Fri Jun 17 21:32:54 EDT 2016'
+            ],
+            'warnings': []
+        }
+    )
 
 
 @pytest.fixture()
