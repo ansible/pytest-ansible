9ac53bbae1ea847319bb33792cffe1a4c19240f2
Geetika Kapoor <13978799+geetikakay@users.noreply.github.com>
2018-07-16 19:37:39 +0530

    Update errors.py

--- pytest_ansible/errors.py
+++ pytest_ansible/errors.py
@@ -15,7 +15,7 @@ class AnsibleConnectionFailure(ansible.errors.AnsibleError):
     """Sub-class AnsibleError when connection failures occur."""
 
     def __init__(self, msg, dark=None, contacted=None):
-        """Initialize plugin."""
+        """Initialize connection error class."""
         super(AnsibleConnectionFailure, self).__init__(msg)
         self.contacted = contacted
         self.dark = dark
