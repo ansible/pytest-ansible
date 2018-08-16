96076bb7f668e7f642449eec6670bab8dbfd5bce
James Laska <jlaska@redhat.com>
2018-08-16 10:33:23 -0400

    Add py37 as supported environment

--- setup.py
+++ setup.py
@@ -136,8 +136,9 @@ setup(
         'Topic :: Utilities',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7 ',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6 ',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
