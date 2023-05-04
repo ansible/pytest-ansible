db7e7cff2a7384d73e849ea97527cc32704ade49
Bradley A. Thornton <bthornto@redhat.com>
2023-05-04 14:42:50 -0700

    Add some unit tests for units functionality

---
+++ tests/unit/test_unit.py
@@ -0,0 +1,83 @@
+"""Tests specific to the unit test functionality."""
+
+
+import logging
+import re
+import subprocess
+import sys
+from pathlib import Path
+
+import pytest
+
+from pytest_ansible.units import inject, inject_only
+
+
+def test_inject(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: Path,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test injecting a path.
+
+    :param monkeypatch: The pytest monkeypatch fixture
+    :param tmp_path: The pytest tmp_path fixture
+    :param caplog: The pytest caplog fixture
+    """
+    caplog.set_level(logging.DEBUG)
+
+    def mock_get_collection_name(start_path: str) -> tuple[str, str]:
+        """Mock the get_collection_name function.
+
+        :param start_path: The path to the root of the collection
+        :returns: A tuple of the namespace and name
+        """
+        return "namespace", "name"
+
+    monkeypatch.setattr(
+        "pytest_ansible.units.get_collection_name", mock_get_collection_name
+    )
+
+    (tmp_path / "collections" / "ansible_collections").mkdir(parents=True)
+
+    inject(tmp_path)
+    assert (
+        tmp_path / "collections" / "ansible_collections" / "namespace" / "name"
+    ).is_dir()
+    assert (
+        re.search(r"_ACF installed: \['(.*)',.*]", caplog.text).groups()[0]
+        == sys.path[0]
+        == str(tmp_path / "collections")
+        == re.search(r"_ACF configured paths: \['(.*)']", caplog.text).groups()[0]
+    )
+
+
+def test_inject_only(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: Path,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    """Test only injecting a path.
+
+    :param monkeypatch: The pytest monkeypatch fixture
+    :param tmp_path: The pytest tmp_path fixture
+    :param caplog: The pytest caplog fixture
+    """
+    caplog.set_level(logging.DEBUG)
+    monkeypatch.setenv("ANSIBLE_COLLECTIONS_PATHS", str(tmp_path / "collections"))
+
+    (tmp_path / "collections" / "ansible_collections").mkdir(parents=True)
+
+    inject_only()
+    assert (
+        re.search(r"_ACF installed: \['(.*)']", caplog.text).groups()[0]
+        == sys.path[0]
+        == str(tmp_path / "collections")
+        == re.search(r"_ACF configured paths: \['(.*)']", caplog.text).groups()[0]
+    )
+
+
+def test_for_params():
+    """Test for params."""
+    proc = subprocess.run("pytest --help", shell=True, capture_output=True, check=False)
+    assert "--ansible-unit" in proc.stdout.decode()
+    assert "--ansible-unit-inject-only" in proc.stdout.decode()
