45405264d700acab276a4247091a56b4c913b40b
Bradley A. Thornton <bthornto@redhat.com>
2023-05-05 04:53:22 -0700

    Enable unit test support by default

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -8,6 +8,7 @@ import sys
 from pathlib import Path
 
 import pytest
+
 from pytest_ansible.units import inject, inject_only
 
 
@@ -79,5 +80,4 @@ def test_inject_only(
 def test_for_params():
     """Test for params."""
     proc = subprocess.run("pytest --help", shell=True, capture_output=True, check=False)
-    assert "--ansible-unit" in proc.stdout.decode()
     assert "--ansible-unit-inject-only" in proc.stdout.decode()
