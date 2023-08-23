b944219d9767b57e9038e23dbc39a49856a83682
Ruchi Pakhle <ruchipakhle@gmail.com>
2023-08-23 11:02:05 +0530

    rename driver delegated to default

--- tests/integration/test_molecule.py
+++ tests/integration/test_molecule.py
@@ -27,7 +27,7 @@ def test_molecule_collect() -> None:
         pytest.fail(exc.stderr)
 
     assert proc.returncode == 0
-    assert "test[delegated]" in proc.stdout
+    assert "test[default]" in proc.stdout
 
 
 def test_molecule_disabled() -> None:
@@ -48,18 +48,22 @@ def test_molecule_disabled() -> None:
 def test_molecule_runtest() -> None:
     """Test running the molecule scenarion via pytest."""
 
-    proc = subprocess.run(
-        f"{sys.executable} -m pytest --molecule tests/fixtures/molecule/default/molecule.yml",
-        capture_output=True,
-        check=True,
-        env={"PATH": os.environ["PATH"]},
-        shell=True,
-        text=True,
-    )
-    assert proc.returncode == 0
-    assert "collected 1 item" in proc.stdout
-    assert "tests/fixtures/molecule/default/molecule.yml::test " in proc.stdout
-    assert "1 passed" in proc.stdout
+    try:
+        proc = subprocess.run(
+            f"{sys.executable} -m pytest --molecule tests/fixtures/molecule/default/molecule.yml",
+            capture_output=True,
+            check=True,
+            env={"PATH": os.environ["PATH"]},
+            shell=True,
+            text=True,
+        )
+        assert proc.returncode == 0
+        assert "collected 1 item" in proc.stdout
+        assert "tests/fixtures/molecule/default/molecule.yml::test " in proc.stdout
+        assert "1 passed" in proc.stdout
+    except subprocess.CalledProcessError as exc:
+        print(exc.stdout)
+        print(exc.stderr)
 
 
 def test_molecule_fixture(molecule_scenario: MoleculeScenario) -> None:
