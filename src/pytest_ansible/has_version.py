c84bdd04ab467899b014fa5b0b8246e0498df22b
Kate Case <kcase@redhat.com>
2025-06-25 10:35:32 -0400

    Dev builds are strictly earlier than .0 release

--- src/pytest_ansible/has_version.py
+++ src/pytest_ansible/has_version.py
@@ -7,12 +7,11 @@ import ansible
 from packaging.version import parse as parse_version
 
 
-has_ansible_v2 = parse_version(ansible.__version__) >= parse_version("2.0.0")
-has_ansible_v24 = parse_version(ansible.__version__) >= parse_version("2.4.0")
-has_ansible_v28 = parse_version(ansible.__version__) >= parse_version(
-    "2.8.0.dev0",
-) or parse_version(ansible.__version__) >= parse_version("2.8.0")
-has_ansible_v29 = parse_version(ansible.__version__) >= parse_version("2.9.0")
-has_ansible_v212 = parse_version(ansible.__version__) >= parse_version("2.12.0")
-has_ansible_v213 = parse_version(ansible.__version__) >= parse_version("2.13.0")
-has_ansible_v219 = parse_version(ansible.__version__) >= parse_version("2.19.0")
+ansible_version = parse_version(ansible.__version__)
+has_ansible_v2 = ansible_version >= parse_version("2.0.0")
+has_ansible_v24 = ansible_version >= parse_version("2.4.0")
+has_ansible_v28 = ansible_version >= parse_version("2.8.0.dev0")
+has_ansible_v29 = ansible_version >= parse_version("2.9.0")
+has_ansible_v212 = ansible_version >= parse_version("2.12.0")
+has_ansible_v213 = ansible_version >= parse_version("2.13.0")
+has_ansible_v219 = ansible_version >= parse_version("2.19.0.dev0")
